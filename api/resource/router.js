6599020b816089d1832f45da1dcd75ccd9ab4a20
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-21 18:45:53 -0700

    get and post functional for resource

--- api/resource/router.js
+++ api/resource/router.js
@@ -4,4 +4,20 @@ const Resource = require('./model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    Resource.getResources()
+        .then(resources => {
+            res.status(200).json(resources)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Resource.createResource(req.body)
+        .then(createdResources => {
+            res.status(200).json(createdResources)
+        })
+        .catch(next)
+})
+
 module.exports = router;
