5e994d9839263c607ed0264cf96c6f36a8c2923c
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-21 18:11:02 -0700

    project model completed

--- api/project/model.js
+++ api/project/model.js
@@ -1,2 +1,26 @@
 // build your `Project` model here
 const db = require('../../data/dbConfig')
+
+function getProjects() {
+    return db('projects');
+}
+
+function getProjectById(project_id) {
+    return db('projects')
+        .where('project_id', project_id)
+        .first();
+}
+
+function createProject(project) {
+    return db('project')
+        .insert(project)
+        .then(([id]) => {
+            return getProjectById(id)
+        })
+}
+
+module.exports = {
+    getProjects,
+    getProjectById,
+    createProject
+}
