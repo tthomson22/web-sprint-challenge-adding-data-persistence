b42ecef4d1b39758bc0f03de746f7c893b591e48
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-21 19:00:02 -0700

    tasks model added

--- api/task/model.js
+++ api/task/model.js
@@ -1,2 +1,26 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig')
+
+function getTasks() {
+    return db('tasks');
+}
+
+function getTaskById(task_id) {
+    return db('tasks')
+        .where('task_id', task_id)
+        .first();
+}
+
+function createTask(task) {
+    return db('tasks')
+        .insert(task)
+        .then(([id]) => {
+            return getTaskById(id)
+        })
+}
+
+module.exports = {
+    getTasks,
+    getTaskById,
+    createTask
+}
