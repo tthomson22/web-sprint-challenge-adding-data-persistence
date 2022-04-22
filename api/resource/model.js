12a83d7ad3587fac9dea2fafc0fd45f845693101
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-21 18:38:06 -0700

    resource model created

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,2 +1,26 @@
 // build your `Resource` model here
 const db = require('../../data/dbConfig')
+
+function getResources() {
+    return db('resources');
+}
+
+function getResourceById(resource_id) {
+    return db('resources')
+        .where('resource_id', resource_id)
+        .first();
+}
+
+function createResource(resource) {
+    return db('resources')
+        .insert(resource)
+        .then(([id]) => {
+            return getResourceById(id)
+        })
+}
+
+module.exports = {
+    getResources,
+    getResourceById,
+    createResource
+}
